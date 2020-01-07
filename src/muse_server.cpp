89c85e39ef22fa69ee0dffdd611b9ccbceaaea02
Ryan Houck <ry.houck@att.net>
2020-01-07 01:15:15 -0500

    Got further on building out the skeleton!

--- src/muse_server.cpp
+++ src/muse_server.cpp
@@ -1,12 +1,49 @@
 #include "muse_server.hpp"
 
 int main(int argc, char** argv) {
-  int last_char;
-  ITEM** items;
-  MENU* menus;
-  WINDOW* win;
+	int last_char;
+	ITEM** items;
+	MENU* menus;
+	WINDOW* win;
 }
 
-void draw(){
+void cleanup(){
+	endwin();
+	exit(0);
+}
 
+void cleanupServ(){
+	//Close socket
+	cleanup();
+}
+
+void refresh(){
+	return;
+}
+
+void backgroundProc(){
+	return;
+}
+
+void updatePort(int new_port){
+	port = new_port;
+}
+
+void addLibPath(char* lib_path){
+	if(lib_path_num < 64){
+		lib_paths[lib_path_num++] = lib_path;
+	}
+}
+
+void removeLibPath(int idx){
+	int i;
+	if(lib_paths[i][0] != '\0'){
+		for(i = idx; lib_paths[i][0] != '\0' && i < lib_path_num; i++){
+			lib_paths[i] = lib_paths[i+1];
+		}
+		if(i == lib_path_num){
+			memset(lib_paths[i], 0, 255);
+		}
+		lib_path_num--;
+	}
 }
--- src/muse_server.hpp
+++ src/muse_server.hpp
@@ -3,19 +3,18 @@
 #include <curses.h>
 #include <menu.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #include "shared.hpp"
 
 #define ARR_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
 
 enum page {
-  MAIN_PAGE,
-  PORT_PAGE,
-  LIBRARY_PAGE,
-  SERVER_PAGE
+	MAIN_PAGE,
+	PORT_PAGE,
+	LIBRARY_PAGE,
+	SERVER_PAGE
 };
-
-int curr_page = MAIN_PAGE;
 
 char* label_arr[][] = {{"1.", "2.", "3.", "4.", "5.", "6."},
                        {"1.", "2."},
@@ -27,9 +26,20 @@ char* option_arr[][] = {{"Start In Background", "Change Port", "Library Location
                        {"Add Path", "Remove Path", "Back"},
                        {"Kill Server", "Run In Background"}};
 
-void* funct_arr[][] = {{(void*)dummy, (void*)dummy, (void*)dummy, (void*)dummy, (void*)dummy, (void*)dummy},
-                       {(void*)dummy, (void*)dummy},
-                       {(void*)dummy, (void*)dummy, (void*)dummy},
-                       {(void*)dummy, (void*)dummy}};
+void* funct_arr[][] = {{(void*)dummy, (void*)dummy, (void*)dummy, (void*)dummy, (void*)dummy, (void*)cleanup},
+                       {(void*)updatePort, (void*)refresh},
+                       {(void*)dummy, (void*)dummy, (void*)refresh},
+                       {(void*)cleanupServ, (void*)backgroundProc}};
+
+int port = 2442;
+char* lib_paths[64];
+char lib_path_num;
+int curr_page = MAIN_PAGE;
+
+void cleanup();
+void cleanupServ();
+void refresh();
+void backgroundProc();
+void updatePort(int new_port);
 
 #endif
