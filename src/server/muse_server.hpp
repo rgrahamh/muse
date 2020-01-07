cfc8ab5aa3f471ebc2e273bf44fe5d929e32ab9a
Ryan Houck <ry.houck@att.net>
2020-01-07 11:21:07 -0500

    Merged bak in with David's most recent changes

--- src/server/muse_server.hpp
+++ src/server/muse_server.hpp
@@ -1,8 +1,16 @@
+#ifndef MUSE_SERVER_HPP
+#define MUSE_SERVER_HPP
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include "../shared.hpp"
+
+#define DEFAULT_PORT 2442
 
 int sockfd;
 
