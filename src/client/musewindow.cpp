52cdf50f51579278022487eebf097ccf6a5c611a
David Fletcher <david_fletcher@taylor.edu>
2020-01-17 12:11:18 -0500

    significant progress made on playing audio and related functions

--- src/client/musewindow.cpp
+++ src/client/musewindow.cpp
@@ -18,10 +18,15 @@ MuseWindow::MuseWindow(QWidget *parent)
     configureTableView(ui->genreView);
     configureTableView(ui->songView);
 
-    // configure song scrubber
-    ui->songProgressSlider->setMinimum(0);
-    ui->songProgressSlider->setMaximum(100);
-    ui->songProgressSlider->setTickInterval(1);
+    // initialize audio library
+    initializeFMOD();
+
+    // stop user from messing with progress bar
+    ui->songProgressSlider->setAttribute(Qt::WA_TransparentForMouseEvents);
+
+    // intialize update timer
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MuseWindow::on_timeout);
 
     // create some test data
     QList<QString> song_titles;
@@ -64,10 +69,14 @@ MuseWindow::MuseWindow(QWidget *parent)
     QSortFilterProxyModel *song_proxy = new QSortFilterProxyModel();
     song_proxy->setSourceModel(song_model);
     ui->songView->setModel(song_proxy);
+
 }
 
 MuseWindow::~MuseWindow()
 {
+    // release audio library resources
+    system->release();
+
     delete ui;
 }
 
@@ -84,6 +93,23 @@ void MuseWindow::configureTableView(QTableView* view) {
     view->horizontalHeader()->setVisible(true);
     view->setVisible(true);
 }
+
+void MuseWindow::initializeFMOD() {
+    result = FMOD::System_Create(&system);      // Create the main system object.
+    if (result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        exit(-1);
+    }
+
+    result = system->init(512, FMOD_INIT_NORMAL, 0);    // Initialize FMOD.
+    if (result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        exit(-1);
+    }
+}
+
 
 void MuseWindow::on_tabWidget_currentChanged(int index)
 {
@@ -108,12 +134,80 @@ void MuseWindow::on_playButton_clicked()
     play_state = !play_state;
     if( play_state ) {
         ui->playButton->setText("Pause");
+
+        bool is_playing = false;
+        song_channel->isPlaying(&is_playing);
+        if( is_playing ) {
+            song_channel->setPaused(false);
+        } else {
+            // try playing a song
+            system->createStream("/home/dfletch/Documents/School/muse/assets/music/summer_is_gone.mp3", FMOD_DEFAULT, NULL, &song_to_play);
+
+            unsigned int song_length;
+            song_to_play->getLength(&song_length, FMOD_TIMEUNIT_MS);
+
+            unsigned int song_length_s = song_length / 1000;
+
+            char* new_label = (char*) calloc(10, sizeof(char));
+            sprintf(new_label, "%d:%02d", song_length_s / 60, song_length_s % 60);
+            ui->songLengthLabel->setText(new_label);
+
+            ui->songProgressSlider->setMinimum(0);
+            ui->songProgressSlider->setMaximum(song_length);
+            ui->songProgressSlider->setValue(0);
+
+            system->playSound(song_to_play, NULL, false, &song_channel);
+            timer->start(75);
+        }
+
+        // song_to_play->release();
+
     } else {
         ui->playButton->setText("Play");
+
+        bool is_playing = false;
+        song_channel->isPlaying(&is_playing);
+        if( is_playing ) {
+            song_channel->setPaused(true);
+        }
     }
 }
 
 void MuseWindow::on_songView_doubleClicked(const QModelIndex &index)
 {
     qDebug() << "Song index selected is: " << index.row() << endl;
+}
+
+void MuseWindow::on_rewindButton_clicked()
+{
+    bool is_playing = false;
+    song_channel->isPlaying(&is_playing);
+    if( is_playing ) {
+        song_channel->setPosition(0, FMOD_TIMEUNIT_MS);
+    }
+}
+
+void MuseWindow::on_timeout() {
+    bool is_playing = false;
+    song_channel->isPlaying(&is_playing);
+    if( is_playing ) {
+        unsigned int position;
+        song_channel->getPosition(&position, FMOD_TIMEUNIT_MS);
+
+        unsigned int position_s = position / 1000;
+
+        char* new_label = (char*) calloc(10, sizeof(char));
+        sprintf(new_label, "%d:%02d", position_s / 60, position_s % 60);
+        ui->songProgressLabel->setText(new_label);
+
+        ui->songProgressSlider->setValue(position);
+    } else {
+        // song has finished playback
+        ui->songProgressSlider->setValue(0);
+        ui->songProgressLabel->setText("0:00");
+        ui->songLengthLabel->setText("-:--");
+        play_state = false;
+        ui->playButton->setText("Play");
+        timer->stop();
+    }
 }
--- src/client/musewindow.h
+++ src/client/musewindow.h
@@ -7,6 +7,10 @@
 #include <QDebug>
 #include <QSortFilterProxyModel>
 #include <QTableView>
+#include <QTimer>
+#include <fmod.hpp>
+#include <fmod_errors.h>
+#include <fmod_common.h>
 
 
 QT_BEGIN_NAMESPACE
@@ -24,15 +28,24 @@ public:
 
 private slots:
     void on_tabWidget_currentChanged(int index);
-
     void on_playButton_clicked();
-
     void on_songView_doubleClicked(const QModelIndex &index);
+    void on_rewindButton_clicked();
+
+    void on_timeout();
 
 private:
     Ui::MuseWindow *ui;
     bool play_state = false;
 
+    QTimer *timer;
+
+    FMOD_RESULT result;
+    FMOD::System *system = NULL;
+    FMOD::Sound* song_to_play;
+    FMOD::Channel* song_channel;
+
     void configureTableView(QTableView* view);
+    void initializeFMOD();
 };
 #endif // MUSEWINDOW_H
